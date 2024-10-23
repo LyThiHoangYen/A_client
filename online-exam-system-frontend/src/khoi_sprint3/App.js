d2957dff7d03027a906d6c7b3e8b750d5dd948bb
KhoiChau204 <khoichau.work@gmail.com>
2024-10-23 13:38:00 +0700

    khoi_sprint3 createForm

---
+++ online-exam-system-frontend/src/khoi_sprint3/App.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import QuizForm from './QuizForm';
+
+function App() {
+  return (
+    <div className="App">
+      <QuizForm />
+    </div>
+  );
+}
+
+export default App;
