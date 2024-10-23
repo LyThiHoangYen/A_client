62fa541cc4d1c390a233177d8c7a36e88693a08b
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-10-23 15:04:07 +0700

    commit index

--- online-exam-system-frontend/src/index.js
+++ online-exam-system-frontend/src/index.js
@@ -1,9 +1,16 @@
-import './index.css';
+// File: src/index.js
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import App from './Teacher/App'; // Đường dẫn đúng đến App.js trong thư mục teacher
+import './Teacher/style.css'; // Nếu bạn muốn sử dụng style.css từ thư mục teacher
+import { BrowserRouter as Router } from 'react-router-dom';
 
-const el = document.getElementById('root');
-const root = ReactDOM.createRoot(el);
-
-root.render(<App />);
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+     <Router>
+        <App />
+    </Router>
+    
+  </React.StrictMode>
+);
