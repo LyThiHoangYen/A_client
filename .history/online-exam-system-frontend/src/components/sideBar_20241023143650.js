640f5d829e0e136e1fbe18e6affc9ff18ad72d4d
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-10-23 14:40:17 +0700

    commit something

--- .history/online-exam-system-frontend/src/components/sideBar_20241023143650.js
+++ .history/online-exam-system-frontend/src/components/sideBar_20241023143650.js
@@ -1,11 +1,11 @@
 import UserProfile from "./userProfile";
 import Button from "./button";
-import Logo from "./logo";
+import Logo
 
 function Sidebar () {
   return (
     <div className="bg-[#4a3aff] text-white w-1/4 p-8 flex flex-col items-center rounded-tr-2xl rounded-br-2xl">
-      <Logo src="/images/logo.png" alt="logo" size="large" />
+      <div className="text-3xl font-bold mb-8">A+ Sian</div>
       <UserProfile 
         name="Lý Thị Hoàng Yến"
         phone="0917917000"
--- .history/online-exam-system-frontend/src/components/sideBar_20241023143743.js
+++ .history/online-exam-system-frontend/src/components/sideBar_20241023143743.js
@@ -5,7 +5,7 @@ import Logo from "./logo";
 function Sidebar () {
   return (
     <div className="bg-[#4a3aff] text-white w-1/4 p-8 flex flex-col items-center rounded-tr-2xl rounded-br-2xl">
-      <Logo src="/images/logo.png" alt="logo" size="large" />
+      <Logo src="../" alt="Logo" size="large" />
       <UserProfile 
         name="Lý Thị Hoàng Yến"
         phone="0917917000"
