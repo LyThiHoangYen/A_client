ad05295734c37225e953a3c1858b34dd998815c5
Nguyen Ba Hung <22520512@gm.uit.edu.vn>
2024-10-23 14:50:22 +0700

    commit something

--- .history/online-exam-system-frontend/src/pages/studentExamPage_20241023144534.js
+++ .history/online-exam-system-frontend/src/pages/studentExamPage_20241023144534.js
@@ -2,14 +2,24 @@ import SidebarRight from "../components/rightSideBarExam";
 import TakeTheExam from "../components/takeTheExam";
 
 function StudentExamPage() {
-  return (
-    <div className="flex h-screen">
+      <div className="flex h-screen w-screen">
+
       <div className="w-3/4 h-full">
-        <TakeTheExam />
-      </div>
-      <div className="w-1/4 h-full">
-        <SidebarRight />
-      </div>
+
+         <TakeTheExam />
+
+        </div>
+
+         <div className="w-1/4 h-full">
+
+            <SidebarRight />
+
+           </div>
+
+          </div>
+
+      <  );
+
     </div>
   );
 }
